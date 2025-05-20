9e257c8ec00eea247b6fb95875cdc5ae688b4a0d
Mako2727 <mkordo1@yahoo.fr>
2025-05-20 20:22:56 +0200

    mise en place du graphique sur la page d accueil
    mise en place du graphique sur la page d accueil

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { HttpClient } from '@angular/common/http';
+
+
+
+interface Participation {
+  id: number;
+  year: number;
+  city: string;
+  medalsCount: number;
+  athleteCount: number;
+}
+
+interface CountryData {
+  id: number;
+  country: string;
+  participations: Participation[];
+}
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    standalone: false
 })
+
+
+
 export class HomeComponent implements OnInit {
+ multi: { name: string; value: number }[] = [];
+  view: [number, number] = [700, 400];
+
+  legend = true;
+  explodeSlices = false;
+  labels = true;
+  doughnut = false;
+  gradient = false;
+
+numberOfCountries!: number;
+numberOfOlympics!: number;
+
+  
   public olympics$: Observable<any> = of(null);
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService,private  http: HttpClient) {}
 
-  ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+ngOnInit(): void {
+  this.olympics$ = this.olympicService.getOlympics();
+
+  this.http.get<CountryData[]>('../assets/mock/olympic.json').subscribe(
+     
+      (data) => {
+        this.multi = data.map((country) => {
+          console.log(JSON.stringify(data));
+          const totalMedals = country.participations.reduce((sum: number, p: any) => sum + p.medalsCount,0);
+          return {name: country.country, value: totalMedals};
+        });
+           this.numberOfCountries = data.length;
+           this.numberOfOlympics = data[0]?.participations?.length || 0;
+      },
+      (error) => {
+        console.error('Erreur lors du chargement des données JSON:', error);
+      }
+    );
   }
 }
+

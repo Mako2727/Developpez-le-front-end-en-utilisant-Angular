de922ed2a04cbc2ec78eea5bc204cbfc0dc02f07
Mako2727 <mkordo1@yahoo.fr>
2025-05-20 10:49:09 +0200

    Modification des objets pour test
    Modification des objets pour test

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,4 +1,12 @@
 // TODO: create here a typescript interface for an olympic country
+import { Participation } from './Participation';
+export class olympic{
+constructor(public id:number,
+            public country: string,
+              public participations: Participation[] = []
+            ){}
+}
+
 /*
 example of an olympic country:
 {
